8ac41b9bbb666c30433a86e9c10f43dbc1edbbe1
Arthur Fusco <fuscoarthur1@gmail.com>
2020-06-23 22:59:28 -0300

    Botão de atualizar adicionado

--- src/main.js
+++ src/main.js
@@ -1,16 +1,23 @@
 let container = document.querySelector('.container');
 let status = document.querySelectorAll('.status-value');
+let btnAtualizar = document.querySelector('.atualizar button');
 
-axios.get('https://kctbh9vrtdwd.statuspage.io/api/v2/components.json')
-.then(response => {
-    let arrStatus = [];
-    for(let i in response.data.components) {
-        if(Number(i) === 3 || Number(i) === 8)
-            continue;
-        
-        arrStatus.push(response.data.components[i].status);
-    }
+function atualizarStatus() {
+    axios.get('https://kctbh9vrtdwd.statuspage.io/api/v2/components.json')
+    .then(response => {
+        let arrStatus = [];
+        for(let i in response.data.components) {
+            if(Number(i) === 3 || Number(i) === 8)
+                continue;
+            
+            arrStatus.push(response.data.components[i].status);
+        }
 
-    for(let i in arrStatus)
-        status[i].innerHTML = arrStatus[i];
-});
+        for(let i in arrStatus)
+            status[i].innerHTML = arrStatus[i];
+    });
+}
+
+atualizarStatus();
+
+btnAtualizar.onclick = atualizarStatus;
